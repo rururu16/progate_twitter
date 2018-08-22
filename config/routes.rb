57d98c50cc93beb1cb47bf4af3f1068c79bdfdcb
rururu16 <ynk618172@gmail.com>
2018-08-22 21:04:33 +0900

    add User About

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-Rails.application.routes.draw do
+Twitter::Application.routes.draw do
+	
+  root  'about#index'
+  match '/about', to:'about#index', via:'get'
+  resources :users
   resources :tweets
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_234731) do
+ActiveRecord::Schema.define(version: 2018_08_22_112413) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "content"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
