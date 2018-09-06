0eb5b8b18137db10fd607f073e231d05bd7fccea
rururu16 <ynk618172@gmail.com>
2018-09-07 06:29:25 +0900

    mod

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.paginamte(page: params[:page])
   end
 
   # GET /users/1
@@ -25,28 +25,29 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
+    file = params[:user][:image]
+    @user.set_image(file)
 
-    respond_to do |format|
-      if @user.save
-        flash[:success] = "Welcome to Twitter!"
-        redirect_to @user
+    # respond_to do |format|
+       if @user.save
+         flash[:success] = "ようこそ！"
+         redirect_to @user
       else
         render 'new'
       end
-    end
+    # end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+   file = params[:user][:image]
+    @user.set_image(file)
+    if @user.update_attributes(user_params)
+      flash[:success] = "プロフィールを更新しました"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -54,7 +55,7 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
-    flash[:success] = "User destroyed."
+    flash[:success] = "ユーザーを削除しました"
     redirect_to users_url
   end
 
